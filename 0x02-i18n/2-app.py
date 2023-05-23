391dd3c433f9b68a5cb518ab0e9e5b735c50943f
Nicholas <nicholasikiroma@gmail.com>
2023-05-23 21:12:06 +0100

    fix requirement fail

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -14,14 +14,13 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
 
 
+@babel.localeselector
 def get_locale() -> str:
     """Retrieves the locale for a web page."""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
-
-
-babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route("/")
